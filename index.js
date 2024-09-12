c79e3e32c17dd5c131fe0f1bf6d7c463f1541b91
Sophia-G-Chan <sophia.gy.chan@gmail.com>
2024-09-11 17:33:51 -0700

    calendar route set up and error route made

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
-import router from './routes/tasks.js'
+import taskRouter from './routes/tasks.js'
+import calendarRouter from './routes/calendar.js'
 
 const app = express();
 const {PORT, CORS_ORIGIN} = process.env;
 
-app.use('/tasks', router);
+app.use('/tasks', taskRouter);
 
+app.use('/calendars', calendarRouter);
+
+app.use('/', (_req, res) => {
+    res.send('Hello, welcome to the back-end server for JourneyTask');
+})
+
+app.use((err, _req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({error: 'Internal Server Error'})
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
